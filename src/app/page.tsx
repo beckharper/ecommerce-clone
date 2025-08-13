eb6c44d39dafc700005486d77f160a94d6872697
Beck Harper <beck@fastmail.com>
2025-08-13 04:14:45 -0400

    fixed tailwind bug, created PriceTag component, finished ProductCard component, more styling with tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
+import ProductCard from "@/components/productCard";
 import { prisma } from "@/lib/db/prisma";
 import Image from "next/image";
 
 export default async function Home() {
   const products = await prisma.product.findMany({ orderBy: { id: "desc" } });
 
-  return <div></div>;
+  return (
+    <div>
+      <ProductCard product={products[0]} />
+    </div>
+  );
 }
