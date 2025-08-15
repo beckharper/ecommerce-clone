93cad256b17b924fa29dbe3335940735f38b953a
Beck Harper <beck@fastmail.com>
2025-08-14 23:41:24 -0400

    created navbar with search and cart dropdown

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import NavBar from "./Navbar/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <NavBar />
         <main className="p-4 max-w-7xl m-auto min-w-[300px]">{children}</main>
       </body>
     </html>
