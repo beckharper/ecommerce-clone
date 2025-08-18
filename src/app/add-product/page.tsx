217e85815af362f434bb7a58c27458d751ce5139
Beck Harper <beck@fastmail.com>
2025-08-18 03:32:38 -0400

    refactored authOptions into separate auth folder to avoid deployment bug

--- src/app/add-product/page.tsx
+++ src/app/add-product/page.tsx
@@ -2,7 +2,7 @@ import FormSubmitButton from "@/components/formSubmitButton";
 import { prisma } from "@/lib/db/prisma";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
-import { authOptions } from "../api/auth/[...nextauth]/route";
+import { authOptions } from "../api/auth/authOptions";
 
 export const metadata = {
   title: "Add Product - eShop",
--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,32 +1,6 @@
-import { mergeAnonCartIntoUserCart } from "@/lib/db/cart";
-import { prisma } from "@/lib/db/prisma";
-import { env } from "@/lib/env";
-import { PrismaAdapter } from "@auth/prisma-adapter";
-import NextAuth, { NextAuthOptions } from "next-auth";
-import { Adapter } from "next-auth/adapters";
-import GoogleProvider from "next-auth/providers/google";
+import NextAuth from "next-auth";
 
-export const authOptions: NextAuthOptions = {
-  adapter: PrismaAdapter(prisma) as Adapter,
-  providers: [
-    GoogleProvider({
-      clientId: env.GOOGLE_CLIENT_ID,
-      clientSecret: env.GOOGLE_CLIENT_SECRET,
-    }),
-  ],
-  callbacks: {
-    session({ session, user }) {
-      session.user.id = user.id;
-      return session;
-    },
-  },
-
-  events: {
-    async signIn({ user }) {
-      await mergeAnonCartIntoUserCart(user.id);
-    },
-  },
-};
+import { authOptions } from "../authOptions";
 
 const handler = NextAuth(authOptions);
 
